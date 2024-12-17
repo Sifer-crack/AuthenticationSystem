29bf68d968d53287d2c4fc3555a099f331c8fb25
Sam Andreev <samandreev19@gmail.com>
2024-12-18 11:36:56 +1300

    Initial Commit, Project Setup

---
+++ src/test/java/com/personalProject/AuthSys/AuthSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProject.AuthSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
