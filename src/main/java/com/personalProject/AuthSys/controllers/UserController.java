6ee2bf9f16c7a3205c589b46a48e832491098750
Sam Andreev <samandreev19@gmail.com>
2024-12-21 18:24:09 +1300

    Created class UserController that will direct upon request

---
+++ src/main/java/com/personalProject/AuthSys/controllers/UserController.java
@@ -0,0 +1,17 @@
+package com.personalProject.AuthSys.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome, this endpoint isn't secure";
+    }
+
+//    @GetMapping("/user/userProfile")
+}
