305201a5992af7a59d25001e32f1c16894411022
Sam Andreev <samandreev19@gmail.com>
2024-12-21 18:16:26 +1300

    Created new class JwtService started developing, haven't finished

---
+++ src/main/java/com/personalProject/AuthSys/service/jwt/JwtService.java
@@ -0,0 +1,10 @@
+package com.personalProject.AuthSys.service.jwt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY   = "TheMostSecureKeyEver";
+
+//    public String extractUsername(String token)
+}
