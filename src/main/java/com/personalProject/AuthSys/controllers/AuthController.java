17e885c233bce7943fdec969c0ee0960c1d39783
Sam Andreev <samandreev19@gmail.com>
2024-12-23 18:20:09 +1300

    Authentication Controller

---
+++ src/main/java/com/personalProject/AuthSys/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.personalProject.AuthSys.controllers;
+
+import com.personalProject.AuthSys.request.auth.AuthenticationRequest;
+import com.personalProject.AuthSys.request.auth.RegistrationRequest;
+import com.personalProject.AuthSys.responce.AuthenticationResponse;
+import com.personalProject.AuthSys.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    @Autowired
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegistrationRequest request){
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
