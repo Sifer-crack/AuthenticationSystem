00cd11b7125e3e2b13f2b3ad5bcbcd998bc8a63d
Sam Andreev <samandreev19@gmail.com>
2025-02-17 15:46:24 +1300

    Registration Request

--- src/main/java/com/personalProject/AuthSys/request/auth/RegistrationRequest.java
+++ src/main/java/com/personalProject/AuthSys/request/auth/RegistrationRequest.java
@@ -1,4 +1,19 @@
 package com.personalProject.AuthSys.request.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationRequest {
+    private String name;
+    private String lastName;
+    private String email;
+    private String phoneNum;
+    private String password;
+    private Integer role;
 }
