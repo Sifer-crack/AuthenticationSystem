30cca27d2117e79d884e9eed54d64dc431f2835a
Sam Andreev <samandreev19@gmail.com>
2025-02-17 15:44:04 +1300

    Authentication Response

--- src/main/java/com/personalProject/AuthSys/responce/AuthenticationResponse.java
+++ src/main/java/com/personalProject/AuthSys/responce/AuthenticationResponse.java
@@ -1,4 +1,14 @@
 package com.personalProject.AuthSys.responce;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationResponse {
+    private String token;
 }
