ee9b818746fb38a8d6032e54c76081eecd0bee2d
Sam Andreev <samandreev19@gmail.com>
2025-02-23 18:58:41 +1300

    start application

--- src/main/java/com/personalProject/AuthSys/AuthSysApplication.java
+++ src/main/java/com/personalProject/AuthSys/AuthSysApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthSysApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-//		SpringApplication.run(AuthSysApplication.class, args);
+		SpringApplication.run(AuthSysApplication.class, args);
 	}
 
 }
